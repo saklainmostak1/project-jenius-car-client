94f7c6ddbef1a3cc05c085a453c394b8830b3897
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-07 05:27:06 +0600

    push

---
+++ src/Router/Routes/Router.js
@@ -0,0 +1,27 @@
+import Main from "../../layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+        ]
+    }
+])
