8c23f76bdbe9a775331b532196c683ea6bad64d3
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-07 08:43:00 +0600

    push

--- src/Router/Routes/Router.js
+++ src/Router/Routes/Router.js
@@ -1,4 +1,5 @@
 import Main from "../../layout/Main";
+import CheckOut from "../../Pages/CheckOut/CheckOut";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
@@ -21,6 +22,11 @@ export const router = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register></Register>
+            },
+            {
+                path: '/checkout/:id',
+                element: <CheckOut></CheckOut>,
+                loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
             },
         ]
     }
